243162db4f3c16658312db8bff9da51bcd0e3049
Diogo S Rocha <dkl.fjyoho@gmail.com>
2021-05-04 20:49:19 -0400

    Projeto Criado

---
+++ backend/src/main/java/com/devsuperior/brunavendas/BrunavendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.brunavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunavendasApplication.class, args);
+	}
+
+}
